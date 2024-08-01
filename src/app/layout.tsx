c22d2efa7426bab862b24b84319c30838c196717
Zafer AYAN <ozcanzaferayan@gmail.com>
2024-08-01 21:44:30 +0300

    [feat] add ThemeProvider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+import { ThemeProvider } from "@/components/theme-provider";
+import { cn } from "@/lib/utils";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -15,8 +17,25 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className="h-full" suppressHydrationWarning>
+      <body
+        className={cn("relative h-full font-sans antialiased", inter.className)}
+      >
+        <main className="relative flex flex-col min-h-screen">
+          {/* <Navbar /> */}
+          <div className="flex-grow flex-1">
+            <ThemeProvider
+              attribute="class"
+              defaultTheme="system"
+              enableSystem
+              forcedTheme="dark"
+              disableTransitionOnChange
+            >
+              {children}
+            </ThemeProvider>
+          </div>
+        </main>
+      </body>
     </html>
   );
 }
